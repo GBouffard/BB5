26f7ab085f8b5ba646b579cf4fa22afc4ab8b83d
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-04-20 12:17:00 +0100

    bike done

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,2 +1,15 @@
 class Bike
+  attr_reader :working
+  def initialize
+    @working = true
+  end
+
+  def break!
+    @working = false
+  end
+
+  def fix!
+    fail 'bike already working'if @working == true
+    @working = true
+  end
 end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,6 +1,23 @@
 require 'bike'
 
 describe Bike do
-  xit '' do
+  bike = Bike.new
+  it 'is working when created' do
+    expect(bike.working).to be true
+  end
+
+  it 'can break' do
+    bike.break!
+    expect(bike.working).to be false
+  end
+
+  it 'can be fixed if broken' do
+    bike.break!
+    bike.fix!
+    expect(bike.working).to be true
+  end
+
+  it 'cannot be fixed if working' do
+    expect { bike.fix! }.to raise_error 'bike already working'
   end
 end
