81877ec64080f09af424177ace826687c6fdffa7
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-04-20 15:37:29 +0100

    van done

--- lib/van.rb
+++ lib/van.rb
@@ -1,2 +1,17 @@
 class Van
+  attr_reader :capacity, :bikes
+  def initialize
+    @capacity = 15
+    @bikes = []
+  end
+
+  def collect(bike, from)
+    @bikes << bike
+    from.bikes.delete(bike)
+  end
+
+  def release(bike, to)
+    @bikes.delete(bike)
+    to.bikes.push(bike)
+  end
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,6 +1,35 @@
 require 'van'
 
 describe Van do
-  xit '' do
+  let(:van) { Van.new }
+  let(:working_bike) { double :bike, working: true }
+  let(:broken_bike) { double :bike, working: false }
+  let(:ds) { double :docking_station, bikes: [working_bike, broken_bike] }
+  # nb: no need to test garage. As long as the collection of garage is called bikes, it should work.
+  it 'has a capacity' do
+    expect(van.capacity).to eq(15)
+  end
+
+  it 'can collect a broken bike from a docking station' do
+    van.collect(broken_bike, ds)
+    expect(van.bikes).to eq [broken_bike]
+  end
+
+  it 'can release a working bike to a docking station' do
+    van.collect(working_bike, ds)
+    van.release(working_bike, ds)
+    expect(van.bikes).to eq []
+  end
+
+  it 'removes a bike from a docking station while collecting' do
+    van.collect(broken_bike, ds)
+    allow(ds).to receive(:collect)
+    expect(ds.bikes).to eq [working_bike]
+  end
+
+  it 'adds a bike to a docking station while releasing' do
+    van.release(working_bike, ds)
+    allow(ds).to receive(:release)
+    expect(ds.bikes).to eq [working_bike, broken_bike, working_bike]
   end
 end
