e67fc828e06813c5e854517db4f2c4cdea5c9c5c
Guillaume Bouffard <guillaume.bouffard@hotmail.co.uk>
2015-04-24 18:21:25 +0100

    grand finale test completed

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-A review of Boris Bikes during lab week.
+A complete review of the Boris Bikes Tutorial from week 1 at Makers Academy. TDD using rspec.
+It includes:
+- unit tests
+- integration tests
+- A grand finale test that I made up to make sure it was all working.
+
+Bike
+  is working when created
+  can break
+  can be fixed if broken
+  cannot be fixed if working
+
+DockingStation
+  has a capacity when created
+  can dock a bike
+  can release a bike
+  cannot dock more bikes than its capacity
+  cannot release a bike if there are no bikes left
+
+Garage
+  has a capacity
+  can dock a bike
+  cannot dock more bikes than its capacity
+  can release a bike
+  cannot release a bike if there are no bikes left
+  removes a bike from the van when docking a bike to self
+  adds a bike to the van station when releasing from self
+  can fix a bike once it's docked
+
+Van
+  has a capacity
+  can only collect a broken bike from a docking station
+  can only collect a working bike from a garage
+  cannot collect more bikes than its capacity
+  can release a bike to a docking station (or a garage)
+  cannot release a bike if there are no bikes left
+  removes a bike from a docking station (or a garage) while collecting
+  adds a bike to a docking station (or a garage) while releasing
+
+Guillaume's own Feature test; a station has 20 bikes
+  they get taken. 4 come back broken. Van collects, garage fixes and van brings back to docking station. They should all work now.
+
+Finished in 0.01633 seconds (files took 0.4822 seconds to load)
+26 examples, 0 failures
--- spec/features/feature_test_spec.rb
+++ spec/features/feature_test_spec.rb
@@ -9,7 +9,7 @@ feature 'Guillaume\'s own Feature test; a station has 20 bikes' do
     ds = DockingStation.new
     20.times { ds.dock(Bike.new) }
     bikeslist = ds.bikes
-    # I just realised that we are missing a user class that allow us to track 
+    # I just realised that we are missing a user class that allow us to track
     # each individual bike when we release them! so if I release as...
     # ds.bikes.each { |bike| ds.release(bike) }
     # we cannot track them! On the other hand, in London, bikes go from left to right
@@ -18,8 +18,29 @@ feature 'Guillaume\'s own Feature test; a station has 20 bikes' do
     bikeslist[7].break!
     bikeslist[11].break!
     bikeslist[19].break!
+    expect(ds.bikes.all?(&:working)).to be false
     van = Van.new
     bikeslist.each { |bike| van.collect_from_ds(bike, ds) }
-    expect(van.bikes.length).to eq (4)
+    expect(van.bikes.length).to eq(4)
+    garage = Garage.new
+    # because .each with a delete inside the code changes the array
+    # as in [0] ok and [1] ok, but then the array has nothing at [2] & [3] because
+    # now this array has a size 2, I had to find another way to iterrate through my bikes.
+    # we load the 4 broken bikes in the van.
+    (van.bikes.length).times { garage.dock(van.bikes[0], van) }
+    expect(van.bikes.length).to eq(0)
+    expect(garage.bikes.length).to eq(4)
+    # the garage fixes them all
+    garage.bikes.each { |bike| garage.fix_a_bike(bike) }
+    # we load them back in the van
+    (garage.bikes.length).times { garage.release(garage.bikes[0], van) }
+    expect(garage.bikes.length).to eq(0)
+    expect(van.bikes.length).to eq(4)
+    # we put them all in the station
+    (van.bikes.length).times { van.release(van.bikes[0], ds) }
+    expect(van.bikes.length).to eq(0)
+    expect(ds.bikes.length).to eq(20)
+    # and they should all be working.
+    expect(ds.bikes.all?(&:working)).to be true
   end
 end
